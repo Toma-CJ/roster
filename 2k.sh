94bca5574994031b2cc877d8e44c18c7b100d14e
Janusz <jawi@itu.dk>
2023-05-16 16:19:26 +0200

    my branch

--- 2k.sh
+++ 2k.sh
@@ -3,7 +3,7 @@
 #export CUDA_DEVICE_ORDER=PCI_BUS_ID
 #export CUDA_VISIBLE_DEVICES=1
 
-CORPUS=test_data
+CORPUS=2k
 SEED=30
 TEMP_DIR=tmp_${CORPUS}_$SEED
 OUT_DIR=out_$CORPUS
@@ -11,8 +11,8 @@ mkdir -p $TEMP_DIR
 mkdir -p $OUT_DIR
 eval "$(conda shell.bash hook)"
 conda activate 2yp
-python -u /home/thoc/RoSTER-main/src/train.py --data_dir /home/thoc/RoSTER-main/data/$CORPUS \
-    --output_dir /home/thoc/RoSTER-main/$OUT_DIR --temp_dir /home/thoc/RoSTER-main/$TEMP_DIR \
+python -u /home/RoSTER/src/train.py --data_dir /home/RoSTER/data/final/$CORPUS \
+    --output_dir /home/RoSTER/$OUT_DIR --temp_dir /home/RoSTER/$TEMP_DIR \
     --pretrained_model roberta-base --tag_scheme 'iob' --max_seq_length 120 \
     --train_batch_size 32 --gradient_accumulation_steps 1 --eval_batch_size 64 \
     --noise_train_lr 3e-5 --ensemble_train_lr 1e-5 --self_train_lr 5e-7 \
@@ -20,6 +20,5 @@ python -u /home/thoc/RoSTER-main/src/train.py --data_dir /home/thoc/RoSTER-main/
     --noise_train_update_interval 60 --self_train_update_interval 100 \
     --dropout 0.1 --warmup_proportion=0.1 --seed $SEED \
     --q 0.7 --tau 0.7 --num_models 5 \
-    --do_train --do_eval --eval_on "test" | tee /home/thoc/RoSTER-main/$OUT_DIR/train_log.txt
+    --do_train --do_eval --eval_on "test" | tee /home/RoSTER/$OUT_DIR/train_log.txt
     
-
