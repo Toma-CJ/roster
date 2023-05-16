7c38912668aa4fd1faea4d9b7e99ae9dccbbb880
Janusz <jawi@itu.dk>
2023-05-16 18:44:23 +0200

    ...

--- baseline.sh
+++ baseline.sh
@@ -21,8 +21,10 @@ TEMP_DIR=tmp_${CORPUS}_$SEED
 OUT_DIR=out_$CORPUS
 mkdir -p $TEMP_DIR
 mkdir -p $OUT_DIR
+
 eval "$(conda shell.bash hook)"
 conda activate 2yp
+
 python -u src/train.py --data_dir data/final/$CORPUS \
     --output_dir $OUT_DIR --temp_dir $TEMP_DIR \
     --pretrained_model roberta-base --tag_scheme 'iob' --max_seq_length 120 \
