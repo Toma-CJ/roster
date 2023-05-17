b720bc715ad3c25fe590e0a3a12e1d4954590239
Janusz <jawi@itu.dk>
2023-05-17 19:55:40 +0200

    ..

--- baseline.sh
+++ baseline.sh
@@ -28,7 +28,7 @@ conda activate 2yp
 python -u src/train.py --data_dir data/final/$CORPUS \
     --output_dir $OUT_DIR --temp_dir $TEMP_DIR \
     --pretrained_model roberta-base --tag_scheme 'iob' --max_seq_length 120 \
-    --train_batch_size 32 --gradient_accumulation_steps 1 --eval_batch_size 64 \
+    --train_batch_size 32 --gradient_accumulation_steps 2 --eval_batch_size 64 \
     --noise_train_lr 3e-5 --ensemble_train_lr 1e-5 --self_train_lr 5e-7 \
     --noise_train_epochs 5 --ensemble_train_epochs 10 --self_train_epochs 5 \
     --noise_train_update_interval 60 --self_train_update_interval 100 \
