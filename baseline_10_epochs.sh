e0df3673de69c7d1751a1e45e46e9c2b8a178329
Janusz <jawi@itu.dk>
2023-05-25 13:35:50 +0200

    20e

--- baseline_10_epochs.sh
+++ baseline_10_epochs.sh
@@ -35,7 +35,7 @@ python -u src/train.py --data_dir data/$CORPUS \
     --pretrained_model roberta-base --tag_scheme 'iob' --max_seq_length 120 \
     --train_batch_size 32 --gradient_accumulation_steps 2 --eval_batch_size 64 \
     --noise_train_lr 3e-5 --ensemble_train_lr 1e-5 --self_train_lr 5e-7 \
-    --noise_train_epochs 5 --ensemble_train_epochs 10 --self_train_epochs 5 \
+    --noise_train_epochs 20 --ensemble_train_epochs 10 --self_train_epochs 5 \
     --noise_train_update_interval 60 --self_train_update_interval 100 \
     --dropout 0.1 --warmup_proportion=0.1 --seed $SEED \
     --q 0.7 --tau 0.7 --num_models 5 \
