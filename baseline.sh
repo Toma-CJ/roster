76aa168688e2c4678749537edc9526240bd21f0d
Janusz <jawi@itu.dk>
2023-06-03 16:01:20 +0200

    added em parameter

--- baseline.sh
+++ baseline.sh
@@ -23,6 +23,11 @@ while [[ $# -gt 0 ]]; do
       shift # past argument
       shift # past value
       ;;
+    -em|--ensemble_models)
+      ENSEMBLE_MODELS="$2"
+      shift # past argument
+      shift # past value
+      ;;
     *)
       echo "Invalid argument: $1"
       shift # past argument
@@ -48,7 +53,7 @@ python -u src/train.py --data_dir data/$CORPUS \
     --noise_train_epochs $TRAIN_EPOCHS --ensemble_train_epochs 10 --self_train_epochs 5 \
     --noise_train_update_interval 60 --self_train_update_interval 100 \
     --dropout 0.1 --warmup_proportion=0.1 --seed $SEED \
-    --q 0.7 --tau 0.7 --num_models 5 \
+    --q 0.7 --tau 0.7 --num_models $ENSEMBLE_MODELS \
     --do_train --do_eval --eval_on "valid" | tee $OUT_DIR/train_log.txt
 
 python -u src/train.py --data_dir data/$EVAL_DATASET \
