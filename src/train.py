05a16780cd84e74efcfeb60b1f83214b376bc0d3
janusz <jawi@itu.dk>
2023-06-18 14:13:45 +0200

    1

--- src/train.py
+++ src/train.py
@@ -191,7 +191,7 @@ def main():
             )
             tuner.fit()
 
-        ray.init(address="auto")
+        ray.init(address="auto", num_cpus = 1 ,num_gpus = 1)
 
         tune_with_setup()
 
